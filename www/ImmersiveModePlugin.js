3a77c6b6b3976c9b3fbe99484d0f7d05fa4a04cf
toluhta <toluhta@users.noreply.github.com>
2014-01-25 13:01:13 +0200

    removed unnecessary nulls

--- www/ImmersiveModePlugin.js
+++ www/ImmersiveModePlugin.js
@@ -1,12 +1,12 @@
     var Immersify = {
         enable : function(successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, "Immersify", "enableImmersive", [null]);
+            cordova.exec(successCallback, errorCallback, "Immersify", "enableImmersive", []);
         },
         enableSticky : function(successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, "Immersify", "enableSticky", [null]);
+            cordova.exec(successCallback, errorCallback, "Immersify", "enableSticky", []);
         },
         disable : function(successCallback, errorCallback) {
-            cordova.exec(successCallback, errorCallback, "Immersify", "disable", [null]);
+            cordova.exec(successCallback, errorCallback, "Immersify", "disable", []);
         }
     }
     module.exports = Immersify;
